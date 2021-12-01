8378ab7f832416c55e43c7903cb11ef583bdd684
Mosè Giordano <mose@gnu.org>
2021-12-01 15:04:25 +0000

    Import `yr` unit from `Unitful` using appropriate macro

--- src/UnitfulAstro.jl
+++ src/UnitfulAstro.jl
@@ -3,7 +3,7 @@ __precompile__()
 module UnitfulAstro
 
 import Unitful
-using Unitful: @unit, @logscale, @logunit, yr
+using Unitful: @unit, @logscale, @logunit
 
 macro import_from_unitful(args...)
     expr = Expr(:block)
@@ -34,7 +34,7 @@ function should_we_use_SI_prefixes(arg::Expr)
 end
 should_we_use_SI_prefixes(arg::Symbol) = false, arg
 
-@import_from_unitful ~m ~s ~A ~K ~cd
+@import_from_unitful ~m ~s ~A ~K ~cd ~yr
 @import_from_unitful ~L ~Hz ~N ~Pa ~J ~W ~C ~V ~Ω ~S ~F ~H ~T ~Wb ~lm ~lx ~Bq ~Gy ~Sv ~kat ~eV
 @import_from_unitful sr rad ° °C °F Ra minute hr d wk ~bar atm Torr
 @import_from_unitful q c0 c μ0 µ0 ε0 ϵ0 Z0 G gn ge h ħ Φ0 me mn mp μB µB Na R k σ
