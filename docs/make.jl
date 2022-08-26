25ee9728447b00a7bfeaa5640013dce96da895da
William Thompson <will.thompson@outlook.com>
2022-08-26 13:16:31 -0700

    Towards unified JuliaAstro docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,13 @@
 using Documenter, UnitfulAstro
 
+include("pages.jl")
 makedocs(
     format = Documenter.HTML(prettyurls = !("local" in ARGS)),
     sitename = "UnitfulAstro.jl",
     authors = "Michael Eastwood",
     linkcheck = true,
     linkcheck_ignore = [r"assets/.+.pdf"],
-    pages = [
-        "Home" => "index.md"
-    ]
+    pages = pages
 )
 
 deploydocs(
